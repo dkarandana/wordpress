c18163309c4b6f35f296dd7823fe70f43d0a9647
dkarandana <dhananjayakarandana@gmail.com>
2018-05-11 22:09:35 +0530

    wp_nav_menu

--- wp-content/themes/cpwd/header.php
+++ wp-content/themes/cpwd/header.php
@@ -28,26 +28,5 @@
 </head>
 <body>
 <header>
-<?php
-
-/**
-  * @example https://codex.wordpress.org/Function_Reference/wp_nav_menu
-  * @example wp_nav_menu
-  * @link https://codex.wordpress.org/Function_Reference/wp_nav_menu
-  */
-      $menu = array(
-        'menu'          => 'main-menu',
-        'menu_class'      => 'menu',
-        'menu_id'         => 'main-menu',
-        'container'       => '',
-        //'container_class' => 'main-menu',
-        //'container_id'    => '',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>'
-      );
-
-      wp_nav_menu( $menu ); ?>
+  <?php include 'main-menu.php';?>
 </header>
